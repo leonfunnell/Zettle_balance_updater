a377b197b951ecdca46615f16887b972d50a46e7
Leon Funnell <leon_funnell@epam.com>
2024-11-20 17:14:52 +0000

    modified:   zip_lambda.sh

--- zip_lambda.sh
+++ zip_lambda.sh
@@ -8,12 +8,12 @@ container_id=$(docker create izettleminbal-lambda)
 
 # Copy the deployment package from the container
 docker cp $container_id:/var/task/izettleminbal.py izettleminbal.py
-docker cp $container_id:/var/task/chromedriver chromedriver
-docker cp $container_id:/var/task/google-chrome-stable_current_x86_64.rpm google-chrome-stable_current_x86_64.rpm
+docker cp $container_id:/usr/bin/chromedriver chromedriver
+docker cp $container_id:/opt/google/chrome/google-chrome google-chrome
 
 # Create the deployment package
-zip -r izettleminbal.zip izettleminbal.py chromedriver google-chrome-stable_current_x86_64.rpm
+zip -r izettleminbal.zip izettleminbal.py chromedriver google-chrome
 
 # Clean up
 docker rm $container_id
-rm chromedriver google-chrome-stable_current_x86_64.rpm
+rm chromedriver google-chrome
