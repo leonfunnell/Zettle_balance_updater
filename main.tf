e64c5f9e0f51a57cf084e74afc507152615f9861
Leon Funnell <leon_funnell@epam.com>
2024-11-20 17:29:27 +0000

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -57,32 +57,33 @@ resource "aws_iam_role" "lambda_role" {
       },
     ]
   })
+}
 
-  inline_policy {
-    name   = "lambda_policy"
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Action = [
-            "logs:CreateLogGroup",
-            "logs:CreateLogStream",
-            "logs:PutLogEvents"
-          ]
-          Effect   = "Allow"
-          Resource = "arn:aws:logs:*:*:*"
-        },
-        {
-          Action = [
-            "s3:PutObject",
-            "s3:GetObject"
-          ]
-          Effect   = "Allow"
-          Resource = "arn:aws:s3:::${aws_s3_bucket.lambda_bucket.bucket}/*"
-        }
-      ]
-    })
-  }
+resource "aws_iam_role_policy" "lambda_policy" {
+  name   = "lambda_policy"
+  role   = aws_iam_role.lambda_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.lambda_bucket.bucket}/*"
+      }
+    ]
+  })
 }
 
 resource "aws_lambda_function" "izettleminbal" {
